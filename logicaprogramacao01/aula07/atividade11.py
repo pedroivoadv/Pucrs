39705628b8d244d8a502dc2b93c4781f1601f703
pedroivoadv <pivo88@gmail.com>
2023-03-30 17:30:16 -0300

    atualização

---
+++ logicaprogramacao01/aula07/atividade11.py
@@ -0,0 +1,12 @@
+#Implementar o popular Jogo da Forca, onde
+#o objetivo é descobrir a palavra escolhida
+#pelo computador:
+#• No início, o computador escolhe uma palavra
+#• A cada jogada, o usuário “chuta” uma letra
+#• Se a letra não existir na palavra, ele perde a
+#jogada
+#• Se a letra existir, ela é exibida na posição correta
+#na palavra
+#• Se completar o desenho do homem na forca, o
+#jogo é encerrado
+
